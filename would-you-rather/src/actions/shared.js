c489352650655365bca0217ee572170cc8e7a38f
JEONGSUJI <wtw9611@gmail.com>
2020-03-28 02:12:42 +0900

    error correction & Initializing the App's Data

--- would-you-rather/src/actions/shared.js
+++ would-you-rather/src/actions/shared.js
@@ -3,12 +3,14 @@ import { receiveUsers } from '../actions/users';
 import { receiveQuestions } from '../actions/questions';
 import { setAuthedUser } from '../actions/authedUser';
 
+const userId = 'sarahedo';
+
 export function handleInitialDate() {
   return dispatch => {
     return getInitialData().then(({ users, questions }) => {
+      dispatch(setAuthedUser(userId));
       dispatch(receiveUsers(users));
       dispatch(receiveQuestions(questions));
-      dispatch(setAuthedUser(user_id ? user_id : null));
     });
   };
 }
