531eef88f9e92fa0adb0a1d385b5bb0810913387
JEONGSUJI <wtw9611@gmail.com>
2020-03-31 21:13:12 +0900

    delete QuestionTab Component, add DashboardComponent Tab UI
    - Tab UI 구현
    - Unanswer와 answer 구분 조건 분기하여 Tab 적용
    - Unanswer와 answer 리스트 없을 시 없다고 표시

--- would-you-rather/src/components/Dashboard.js
+++ would-you-rather/src/components/Dashboard.js
@@ -1,29 +1,74 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Question from './Question';
+import { Tab } from 'semantic-ui-react';
 
 class Dashboard extends Component {
   render() {
+    const panes = [
+      {
+        menuItem: 'Unanswered Questions',
+        render: () => (
+          <Tab.Pane attached={false}>
+            {this.props.unAnsweredList.length === 0 ? (
+              <li key="1">No Unanswered Questions :)</li>
+            ) : (
+              this.props.unAnsweredList.map(id => (
+                <li key={id}>
+                  <Question id={id} />
+                </li>
+              ))
+            )}
+          </Tab.Pane>
+        )
+      },
+      {
+        menuItem: 'Answered Questions',
+        render: () => (
+          <Tab.Pane attached={false}>
+            {this.props.unAnsweredList.length === 0 ? (
+              <li key="1">No Answered Questions :)</li>
+            ) : (
+              this.props.answeredList.map(id => (
+                <li key={id}>
+                  <Question id={id} />
+                </li>
+              ))
+            )}
+          </Tab.Pane>
+        )
+      }
+    ];
+
     return (
       <div className="content">
-        <ul>
-          {this.props.questionsIds.map(id => (
-            <li key={id}>
-              <Question id={id} />
-            </li>
-          ))}
-        </ul>
+        <Tab menu={{ attached: false, tabular: false }} panes={panes}></Tab>
       </div>
     );
   }
 }
 
-function mapStateToProps({ questions }) {
+function mapStateToProps({ authedUser, questions }) {
   return {
-    questionsIds: Object.keys(questions).sort(
-      (a, b) => questions[b].timestamp - questions[a].timestamp
-    )
+    answeredList: Object.keys(questions)
+      .filter(
+        id =>
+          (questions[id].optionOne['votes'].includes(authedUser) ||
+            questions[id].optionTwo['votes'].includes(authedUser)) === true
+      )
+      .sort((a, b) => questions[b].timestamp - questions[a].timestamp),
+    unAnsweredList: Object.keys(questions)
+      .filter(
+        id =>
+          (questions[id].optionOne['votes'].includes(authedUser) ||
+            questions[id].optionTwo['votes'].includes(authedUser)) === false
+      )
+      .sort((a, b) => questions[b].timestamp - questions[a].timestamp)
   };
 }
+
+// hasAnswered:
+//       optionOne['votes'].includes(authedUser) ||
+//       optionTwo['votes'].includes(authedUser),
 
 export default connect(mapStateToProps)(Dashboard);
